4ac3fea9be0825ed03c6d5ece35d6015ef8a239c
sanjeev <sanjeev25bhatt@gmail.com>
2015-10-25 22:03:23 +0530

    aman made changes

---
+++ users.sql
@@ -0,0 +1,68 @@
+-- phpMyAdmin SQL Dump
+-- version 4.2.11
+-- http://www.phpmyadmin.net
+--
+-- Host: 127.0.0.1
+-- Generation Time: Oct 25, 2015 at 05:30 PM
+-- Server version: 5.6.21
+-- PHP Version: 5.6.3
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Database: `a_database`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE IF NOT EXISTS `users` (
+`id` int(11) NOT NULL,
+  `firstname` varchar(40) NOT NULL,
+  `surname` varchar(40) NOT NULL,
+  `email` varchar(65) NOT NULL,
+  `phone` varchar(10) NOT NULL,
+  `password` varchar(40) NOT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `firstname`, `surname`, `email`, `phone`, `password`) VALUES
+(6, 'ashutosh', 'sharma', 'iamashu2002@gmail.com', '9410920247', '70aea0429bf46d1b922e48e1af58dd33'),
+(7, 'sunita', 'sharma', 'sonu@gmail.com', '9557373673', '1c6ac5a840bb6d1f3120a59fe3e13ecc'),
+(8, 'aman', 'sharma', 'aman.asvickes.sharma45@gmail.com', '9557373673', '7ca6feed54eb0ac5dd2e67cc40f21f27');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+ ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=9;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;

560d8a95a06261121a928df5568f5f175c106e30
sanjeev <sanjeev25bhatt@gmail.com>
2016-01-20 21:50:32 +0530

    database

--- a_database.sql
+++ a_database.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Nov 26, 2015 at 04:01 PM
+-- Generation Time: Jan 20, 2016 at 05:18 PM
 -- Server version: 5.6.21
 -- PHP Version: 5.6.3
 
@@ -28,17 +28,40 @@ SET time_zone = "+00:00";
 
 CREATE TABLE IF NOT EXISTS `admin` (
 `id` int(11) NOT NULL,
-  `adminname` varchar(40) NOT NULL,
-  `password` varchar(40) NOT NULL,
-  `online` tinyint(1) NOT NULL DEFAULT '0'
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+  `adminname` varchar(25) NOT NULL,
+  `password` varchar(35) NOT NULL,
+  `online` int(11) NOT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `admin`
 --
 
 INSERT INTO `admin` (`id`, `adminname`, `password`, `online`) VALUES
-(1, 'admin', '6dad2b2478af51e592fd7150bce02ccb', 1);
+(1, 'admin', '6dad2b2478af51e592fd7150bce02ccb', 0),
+(2, 'admin2', '9fcca8a6f2920f02c0b7b49b140b1d36', 0);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `admin_status`
+--
+
+CREATE TABLE IF NOT EXISTS `admin_status` (
+`id` int(11) NOT NULL,
+  `admin_id` int(11) NOT NULL,
+  `uid11` int(11) NOT NULL,
+  `uid12` int(11) NOT NULL,
+  `uid13` int(11) NOT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `admin_status`
+--
+
+INSERT INTO `admin_status` (`id`, `admin_id`, `uid11`, `uid12`, `uid13`) VALUES
+(1, 1, 0, 0, 0),
+(2, 2, 0, 0, 0);
 
 -- --------------------------------------------------------
 
@@ -51,8 +74,27 @@ CREATE TABLE IF NOT EXISTS `chat` (
   `user-id` int(11) NOT NULL,
   `admin-id` int(11) NOT NULL,
   `chat-text` varchar(500) NOT NULL,
-  `by` varchar(20) NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=52 DEFAULT CHARSET=latin1;
+  `by` varchar(10) NOT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=14 DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `chat`
+--
+
+INSERT INTO `chat` (`id`, `user-id`, `admin-id`, `chat-text`, `by`) VALUES
+(1, 13, 2, 'hi admin', 'user'),
+(2, 14, 2, 'hi there', 'admin'),
+(3, 12, 2, 'hi there', 'admin'),
+(4, 13, 2, 'hi there', 'admin'),
+(5, 14, 2, 'i am vicky', 'user'),
+(6, 14, 2, 'i got it', 'admin'),
+(7, 12, 2, 'i am aman', 'user'),
+(8, 13, 2, 'i am ashu', 'user'),
+(9, 12, 2, 'i got it', 'admin'),
+(10, 13, 2, 'i got it', 'admin'),
+(11, 14, 2, 'yo', 'admin'),
+(12, 12, 2, 'yo', 'admin'),
+(13, 13, 2, 'yo man', 'admin');
 
 -- --------------------------------------------------------
 
@@ -60,7 +102,7 @@ CREATE TABLE IF NOT EXISTS `chat` (
 -- Table structure for table `map-markers`
 --
 
-CREATE TABLE IF NOT EXISTS `map_markers` (
+CREATE TABLE IF NOT EXISTS `map-markers` (
 `id` int(11) NOT NULL,
   `description` varchar(25) NOT NULL,
   `name` varchar(25) NOT NULL,
@@ -73,7 +115,7 @@ CREATE TABLE IF NOT EXISTS `map_markers` (
 -- Dumping data for table `map-markers`
 --
 
-INSERT INTO `map_markers` (`id`, `description`, `name`, `latitude`, `longitude`, `content`) VALUES
+INSERT INTO `map-markers` (`id`, `description`, `name`, `latitude`, `longitude`, `content`) VALUES
 (1, 'restaurant', 'PRAKASH RESTAURANT', 29.86968, 77.890167, 'a high class restaurant of roorkee\r\ngood facilities and food in reasonable price'),
 (2, 'bar', 'HOTEL ROYAL PALACE', 29.869081, 77.889277, 'its a restaurant with a bar and a hotel..\r\nHere chicken is really awesome.'),
 (3, 'restaurant', 'PANJABI DHABA', 29.864084, 77.889197, 'another non veg venue'),
@@ -129,14 +171,16 @@ CREATE TABLE IF NOT EXISTS `users` (
   `email` varchar(65) NOT NULL,
   `phone` varchar(10) NOT NULL,
   `password` varchar(40) NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=15 DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `users`
 --
 
 INSERT INTO `users` (`id`, `firstname`, `surname`, `email`, `phone`, `password`) VALUES
-(12, 'aman', 'sharma', 'aman@gmail.com', '9557373673', '7ca6feed54eb0ac5dd2e67cc40f21f27');
+(12, 'aman', 'sharma', 'aman@gmail.com', '9557373673', '7ca6feed54eb0ac5dd2e67cc40f21f27'),
+(13, 'ashu', 'sharma', 'ashu@gmail.com', '9557373383', 'a510dbdb455263aef31c9b7cf3c6830b'),
+(14, 'vicky', 'sharma', 'vicky@gmail.com', '9745863210', 'e3882d06140f056587ff6c9af39ba7d6');
 
 --
 -- Indexes for dumped tables
@@ -149,6 +193,12 @@ ALTER TABLE `admin`
  ADD PRIMARY KEY (`id`);
 
 --
+-- Indexes for table `admin_status`
+--
+ALTER TABLE `admin_status`
+ ADD PRIMARY KEY (`id`);
+
+--
 -- Indexes for table `chat`
 --
 ALTER TABLE `chat`
@@ -157,7 +207,7 @@ ALTER TABLE `chat`
 --
 -- Indexes for table `map-markers`
 --
-ALTER TABLE `map_markers`
+ALTER TABLE `map-markers`
  ADD PRIMARY KEY (`id`);
 
 --
@@ -186,16 +236,21 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `admin`
 --
 ALTER TABLE `admin`
-MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=2;
+MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=3;
+--
+-- AUTO_INCREMENT for table `admin_status`
+--
+ALTER TABLE `admin_status`
+MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=3;
 --
 -- AUTO_INCREMENT for table `chat`
 --
 ALTER TABLE `chat`
-MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=52;
+MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=14;
 --
 -- AUTO_INCREMENT for table `map-markers`
 --
-ALTER TABLE `map_markers`
+ALTER TABLE `map-markers`
 MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=5;
 --
 -- AUTO_INCREMENT for table `payments`
@@ -211,7 +266,7 @@ MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=3;
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=13;
+MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=15;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;

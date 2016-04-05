9348a9daf3660f04dadd558db71073252b497c89
sanjeev25bhatt <sanjeev25bhatt@gmail.com>
2016-04-05 21:19:23 +0530

    changed index.html

--- CorporateWebsite/Soft Nerve/index.php
+++ CorporateWebsite/Soft Nerve/index.php
@@ -50,7 +50,7 @@
             <span class="icon-bar"></span>
             <span class="icon-bar"></span>
           </button>
-          <a class="navbar-brand" href="index.html">Soft Nerve</a>
+          <a class="navbar-brand" href="index.php">Soft Nerve</a>
         </div>
 
         <!-- Collect the nav links, forms, and other content for toggling -->
